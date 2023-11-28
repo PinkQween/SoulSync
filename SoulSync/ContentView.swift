a61deb29f42f706191ba4b2038bb0f01362503b0
Hanna Skairipa <hanna_boone@icloud.com>
2023-11-27 18:03:56 -0700

    Initial Commit

---
+++ SoulSync/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SoulSync
+//
+//  Created by Hanna Skairipa on 11/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SoulSync/SoulSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoulSyncApp.swift
+//  SoulSync
+//
+//  Created by Hanna Skairipa on 11/27/23.
+//
+
+import SwiftUI
+
+@main
+struct SoulSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
